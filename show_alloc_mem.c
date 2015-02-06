88d257ac4aa75dcbb55f6fb12266883ada541cb1
Antoine Buchser <antoine.buchser@epitech.eu>
2015-02-06 11:48:51 +0100

    fix show alloc mem

--- show_alloc_mem.c
+++ show_alloc_mem.c
@@ -5,7 +5,7 @@
 ** Login   <buchse_a@epitech.net>
 ** 
 ** Started on  Tue Jan 27 15:23:33 2015 Antoine Buchser
-** Last update Fri Jan 30 16:29:48 2015 Antoine Buchser
+** Last update Fri Feb  6 11:42:37 2015 Antoine Buchser
 */
 
 #include <stdio.h>
@@ -16,14 +16,17 @@ void	show_alloc_mem(void)
 {
   t_blk	*tmp;
 
-  printf("break : 0x%05X\n", (unsigned int)((long int)sbrk(0)));
+  printf("break : 0x%X\n", (unsigned int)((long int)sbrk(0)));
   tmp = g_root;
   while (tmp)
     {
-      printf("0x%05X - 0x%05X : %u octets\n",
-	     (unsigned int)((long int)tmp),
-	     (unsigned int)((long int)(tmp + tmp->size)),
-	     (unsigned int)tmp->size);
+      if (!(tmp->free))
+	{
+	  printf("0x%X - 0x%X : %u octets\n",
+		 (unsigned int)((long int)tmp),
+		 (unsigned int)((long int)(tmp + tmp->size)),
+		 (unsigned int)tmp->size);
+	}
       tmp = tmp->next;
     }
 }
